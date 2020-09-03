4182696668ededa5066359bc6c2cdd28c29d22bd
Niraj Patel <786nirajpatel@gmail.com>
2020-09-03 00:53:39 -0400

    Switched form SQLite to PostgreSQL

--- backend.py
+++ backend.py
@@ -1,23 +1,26 @@
-import sqlite3
+import psycopg2
+
 
 class Database():
+    connection_details = ""
     def __init__(self):
-        con = sqlite3.connect("Repository.db")
+        con = psycopg2.connect(self.connection_details)
         cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS images(id INTEGER AUTO_INCREMENT PRIMARY KEY, name TEXT, img BLOB)")
+        cur.execute("CREATE TABLE IF NOT EXISTS images(id SERIAL PRIMARY KEY, name TEXT, img BYTEA)")
         con.commit()
         con.close()
 
     def insert(self, filename, image):
         #print(filename, image)
-        con = sqlite3.connect("Repository.db")
+        con = psycopg2.connect(self.connection_details)
         cur = con.cursor()
-        cur.execute("INSERT INTO images(name, img) VALUES(?,?)", (filename, sqlite3.Binary(image)))
+        #cur.execute("INSERT INTO images VALUES (NULL,?,?)", (filename, image))
+        cur.execute("INSERT INTO images(name, img) VALUES(%s,%s)", (filename, image))
         con.commit()
         con.close()
 
     def view(self):
-        con = sqlite3.connect("Repository.db")
+        con = psycopg2.connect(self.connection_details)
         cur = con.cursor()
         cur.execute("SELECT * FROM images")
         rows = cur.fetchall()
